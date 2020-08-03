c642b30465e17bf4b1a9c588631dce23ae140570
chao.li <chao.li@team-lab.cn>
2020-08-03 16:13:51 +0800

    Initial Commit

---
+++ OpenGL_ES_GLSL_02/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES_GLSL_02
+//
+//  Created by tlab on 2020/8/3.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
